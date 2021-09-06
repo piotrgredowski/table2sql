3b6e9888cc301f4da31c269b95c6e01a7d1053ee
Piotr Grędowski <piotrgredowski@gmail.com>
2021-09-06 18:53:51 +0300

    Make help of CLI better. Also fix some types.

--- table2sql/converters/from_csv.py
+++ table2sql/converters/from_csv.py
@@ -1,6 +1,7 @@
 import csv
+from typing import Any, List, Tuple
 
 
-def get_list_of_tuples_from_csv(path_to_file: str, delimiter=","):
+def get_list_of_tuples_from_csv(path_to_file: str, delimiter=",") -> List[Tuple[Any, ...]]:
     input_file_ = open(path_to_file)
     return list(tuple(row) for row in csv.reader(input_file_, delimiter=delimiter))
